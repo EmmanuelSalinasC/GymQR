2c1e0b8999986442063dfe2db5f93a83f145f17a
SALINITAS <SALINITAS@SALINASGM>
2024-11-11 11:37:40 -0600

    Agregar archivos de proyecto.

---
+++ GymPrueba1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace GymPrueba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelidCliente = new System.Windows.Forms.Label();
+            this.buttonGenerarQR = new System.Windows.Forms.Button();
+            this.pictureBoxQR = new System.Windows.Forms.PictureBox();
+            this.textBoxCliente = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelidCliente
+            // 
+            this.labelidCliente.AutoSize = true;
+            this.labelidCliente.Location = new System.Drawing.Point(82, 216);
+            this.labelidCliente.Name = "labelidCliente";
+            this.labelidCliente.Size = new System.Drawing.Size(64, 16);
+            this.labelidCliente.TabIndex = 0;
+            this.labelidCliente.Text = "ID Cliente";
+            // 
+            // buttonGenerarQR
+            // 
+            this.buttonGenerarQR.Location = new System.Drawing.Point(67, 169);
+            this.buttonGenerarQR.Name = "buttonGenerarQR";
+            this.buttonGenerarQR.Size = new System.Drawing.Size(99, 26);
+            this.buttonGenerarQR.TabIndex = 2;
+            this.buttonGenerarQR.Text = "Generar QR";
+            this.buttonGenerarQR.UseVisualStyleBackColor = true;
+            this.buttonGenerarQR.Click += new System.EventHandler(this.buttonGenerarQR_Click);
+            // 
+            // pictureBoxQR
+            // 
+            this.pictureBoxQR.Location = new System.Drawing.Point(226, 111);
+            this.pictureBoxQR.Name = "pictureBoxQR";
+            this.pictureBoxQR.Size = new System.Drawing.Size(212, 195);
+            this.pictureBoxQR.TabIndex = 3;
+            this.pictureBoxQR.TabStop = false;
+            this.pictureBoxQR.Click += new System.EventHandler(this.pictureBoxQR_Click);
+            // 
+            // textBoxCliente
+            // 
+            this.textBoxCliente.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBoxCliente.Location = new System.Drawing.Point(67, 256);
+            this.textBoxCliente.Name = "textBoxCliente";
+            this.textBoxCliente.Size = new System.Drawing.Size(100, 22);
+            this.textBoxCliente.TabIndex = 4;
+            this.textBoxCliente.TextChanged += new System.EventHandler(this.textBoxCliente_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxCliente);
+            this.Controls.Add(this.pictureBoxQR);
+            this.Controls.Add(this.buttonGenerarQR);
+            this.Controls.Add(this.labelidCliente);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelidCliente;
+        private System.Windows.Forms.Button buttonGenerarQR;
+        private System.Windows.Forms.PictureBox pictureBoxQR;
+        private System.Windows.Forms.TextBox textBoxCliente;
+    }
+}
+
---
+++ GymPrueba1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using QRCoder;
+
+namespace GymPrueba1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializePictureBox();
+        }
+
+        private void InitializePictureBox()
+        {
+            pictureBoxQR.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxQR.Width = 250;
+            pictureBoxQR.Height = 250;
+        }
+
+        private void buttonGenerarQR_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxCliente.Text))
+            {
+                MessageBox.Show("Por favor, ingresa un ID de cliente.");
+                return;
+            }
+            string connectionString = "Server=localhost;Port=3306;Database=Gym;User ID=root;Password=25122004;";
+            string query = "SELECT Cli_Id, Cli_Nombre, Cli_Edad, Cli_Telefono, Cli_Telefono_Emer, Cli_Correo, Est_id, Fecha_Creacion, Fecha_termina FROM Cliente WHERE Cli_Id = @CliId";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@CliId", textBoxCliente.Text);
+                try
+                {
+                    connection.Open();
+                    MySqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        int cliId = Convert.ToInt32(reader["Cli_Id"]);
+                        string nombre = reader["Cli_Nombre"].ToString();
+                        int edad = Convert.ToInt32(reader["Cli_Edad"]);
+                        string telefono = reader["Cli_Telefono"].ToString();
+                        string telefonoEmergencia = reader["Cli_Telefono_Emer"].ToString();
+                        string correo = reader["Cli_Correo"].ToString();
+                        int estId = Convert.ToInt32(reader["Est_id"]);
+                        DateTime fechaCreacion = Convert.ToDateTime(reader["Fecha_Creacion"]);
+                        DateTime? fechaTermina = reader["Fecha_termina"] as DateTime?;
+                        if (estId == 1)
+                        {
+                            DateTime fechaActual = DateTime.Now;
+                            int diasRestantes = (fechaTermina.HasValue ? (fechaTermina.Value - fechaActual).Days : 0);
+                            string qrData = $"ID: {cliId}|Nombre: {nombre}|Edad: {edad}|Telefono: {telefono}|" +
+                                            $"Telefono Emergencia: {telefonoEmergencia}|Correo: {correo}|" +
+                                            $"Estado: MIEMBRO|Fecha Creacion: {fechaCreacion}|Fecha Termina: " +
+                                            $"{(fechaTermina.HasValue ? fechaTermina.Value.ToString("yyyy-MM-dd") : "N/A")}|Dias Restantes: {diasRestantes}";
+                            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+                            {
+                                QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrData, QRCodeGenerator.ECCLevel.Q);
+                                using (QRCode qrCode = new QRCode(qrCodeData))
+                                {
+                                    Bitmap qrCodeImage = qrCode.GetGraphic(20);
+                                    pictureBoxQR.Image = qrCodeImage;
+                                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                                    saveFileDialog.Filter = "PNG Image|*.png";
+                                    saveFileDialog.Title = "Guardar Código QR";
+                                    saveFileDialog.FileName = $"{nombre}_QRCode.png";
+                                    saveFileDialog.InitialDirectory = @"C:\Users\SALINITAS\Pictures\QR Gym";
+                                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                                    {
+                                        qrCodeImage.Save(saveFileDialog.FileName);
+                                        MessageBox.Show("Código QR guardado exitosamente.");
+                                    }
+                                }
+                            }
+                        }
+                        else if (estId == 2)
+                        {
+                            MessageBox.Show("El cliente es un visitante. No se generará un código QR.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cliente no encontrado.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void UpdateFechaTermina(int cliId)
+        {
+            string connectionString = "Server=localhost;Port=3306;Database=GymPrueba1;User ID=root;Password=25122004;";
+            string updateQuery = @" UPDATE Cliente SET Fecha_termina = DATE_SUB(Fecha_termina, INTERVAL 1 DAY) WHERE Cli_Id = @CliId";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand command = new MySqlCommand(updateQuery, connection);
+                command.Parameters.AddWithValue("@CliId", cliId);
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al actualizar la fecha de terminación: " + ex.Message);
+                }
+            }
+        }
+
+        private void pictureBoxQR_Click(object sender, EventArgs e)
+        {
+            if (pictureBoxQR.Image != null)
+            {
+                string[] qrDataParts = ExtractDataFromQRCode(pictureBoxQR.Image);
+                if (qrDataParts.Length > 0)
+                {
+                    int cliId;
+                    if (int.TryParse(qrDataParts[0].Split(':')[1].Trim(), out cliId))
+                    {
+                        UpdateFechaTermina(cliId);
+                        ShowRemainingDays(cliId);
+                    }
+                }
+            }
+        }
+
+        private string[] ExtractDataFromQRCode(Image qrImage)
+        {
+            return new[] { "ID: 1" };
+        }
+
+        private void ShowRemainingDays(int cliId)
+        {
+            string connectionString = "Server=localhost;Port=3306;Database=GymPrueba1;User ID=root;Password=25122004;";
+            string query = "SELECT Fecha_termina FROM Cliente WHERE Cli_Id = @CliId";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@CliId", cliId);
+                try
+                {
+                    connection.Open();
+                    DateTime? fechaTermina = command.ExecuteScalar() as DateTime?;
+                    if (fechaTermina.HasValue)
+                    {
+                        int diasRestantes = (fechaTermina.Value - DateTime.Now).Days;
+                        MessageBox.Show($"Información del Cliente:\n" +
+                                        $"ID: {cliId}\n" +
+                                        $"Dias Restantes: {diasRestantes}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al obtener días restantes: " + ex.Message);
+                }
+            }
+        }
+
+        private void textBoxCliente_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
